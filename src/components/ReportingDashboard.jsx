971751504e1af97e126bbf3ed74329e07f3b41b0
Zhetchko Vitchev <notargetbg@gmail.com>
2018-05-11 16:05:41 +0300

    Redux, React-redux + test reducer

--- src/components/ReportingDashboard.jsx
+++ src/components/ReportingDashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './styles/app.scss';
 
 class ReportingDashboard extends React.Component {
 		
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import mainReducer from './Store/reducers/index';
+
 import ReportingDashboard from './components/ReportingDashboard';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    mainReducer, 
+    composeEnhancers()
+);
+
 ReactDOM.render(
-    <ReportingDashboard />, 
-    document.getElementById('reporting-dashboard')
+    <Provider store={store}>
+        <ReportingDashboard />
+    </Provider>, document.getElementById('reporting-dashboard')
 );
