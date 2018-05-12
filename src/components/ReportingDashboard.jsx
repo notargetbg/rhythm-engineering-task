957561e4f8008a877f806d53fb70844782038f68
Zhetchko Vitchev <notargetbg@gmail.com>
2018-05-12 20:19:18 +0300

    Very basic chart done by example

--- src/components/ReportingDashboard.jsx
+++ src/components/ReportingDashboard.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Chart from './Chart';
+
+import ExampleData from '../store/data.json';
 
 class ReportingDashboard extends React.Component {
 		
 	render() {
 		return (
 			<div className='container'>
-                Hi there, App's setup is done!
+					<div className='header'>
+						<h2>d3 test chart</h2>
+					</div>
+					<div>
+						<Chart data={ExampleData} size={[500,500]} />
+					</div>
 			</div>
 		);
 	}
