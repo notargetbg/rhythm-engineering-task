2276785bb1ecb180527276ab59b0b4d77e6c2964
Zhetchko Vitchev <notargetbg@gmail.com>
2018-05-13 19:35:06 +0300

    Chart & data updates
    -Json structure changed
    -Chart updated
    -Added some headings
    -Added text labels

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { scaleLinear } from "d3-scale";
 import * as d3 from "d3";
 
 class Chart extends React.Component {    
@@ -16,41 +15,61 @@ class Chart extends React.Component {
         }
 
         getMax(data) {
-            return data[0].data.map((dataItem) => dataItem.alcohol_related_crashes);
+            return data.map((dataItem) => dataItem.alcohol_related_crashes);
         }
 
         createBarChart() {
+            const { table } = this.props.data;
+            const [ sizeW, sizeH ] = this.props.size;
+
             const node = this.node;
-            const dataMax = d3.max(this.getMax(this.props.data));
-            const yScale = scaleLinear()
+            const dataMax = d3.max(this.getMax(table));
+            const yScale = d3.scaleLinear()
                 .domain([0, dataMax])
-                .range([0, this.props.size[1]])
+                .range([0, sizeH]);
+
+            const itemW = sizeW / table.length;
+                
             d3.select(node)
-            .selectAll('rect')
-            .data(this.props.data[0].data)
-            .enter()
-            .append('rect');
+                .selectAll('rect')
+                .data(table)
+                .enter()
+                .append('rect');
             
             d3.select(node)
-            .selectAll('rect')
-            .data(this.props.data[0].data)
-            .exit()
-            .remove();
-            
+                .selectAll('rect')
+                .data(table)
+                .style('fill', '#fe9922')
+                .attr('x', (d,i) => i * 100)
+                .attr('y', d => sizeH - yScale(d.alcohol_related_crashes))
+                .attr('height', d => yScale(d.alcohol_related_crashes))
+                .attr('width', itemW - 10);
+
             d3.select(node)
-            .selectAll('rect')
-            .data(this.props.data[0].data)
-            .style('fill', '#fe9922')
-            .attr('x', (d,i) => i * 25)
-            .attr('y', d => this.props.size[1] - yScale(d.alcohol_related_crashes))
-            .attr('height', d => yScale(d.alcohol_related_crashes))
-            .attr('width', 20);
+                .selectAll('text')
+                .data(table)
+                .enter()
+                .append('text')
+                .attr('text-anchor', 'middle')
+                .attr('x', (d,i) => (i * 100) + 45)
+                .attr('y', d => sizeH - yScale(d.alcohol_related_crashes) + 20)
+                .attr("font-family", "Roboto, sans-serif")
+                .attr("font-size", "15px")
+                .attr("fill", "white")
+                .text( (item) => { 
+                    return item.year;
+                })
         }
      render() {
            return (
-            <svg ref={node => this.node = node}
-                width={500} height={500}>
-            </svg>
+            <div>
+                <h3 className="chart-title">
+                    Alcohol Related Crashes
+                </h3>
+                <svg ref={node => this.node = node}
+                    width={500} height={500}>
+                </svg>
+            </div>
            )
         }
      
--- src/components/ReportingDashboard.jsx
+++ src/components/ReportingDashboard.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Chart from './Chart';
 
-import ExampleData from '../store/data.json';
+import alcohol_related_crash_history from '../store/alcohol_related_crash_history.json';
 
 class ReportingDashboard extends React.Component {
 		
 	render() {
 		return (
 			<div className='container'>
-					<div className='header'>
-						<h2>d3 test chart</h2>
+					<div className='text-center'>
+						<h2>{alcohol_related_crash_history.name}</h2>
 					</div>
 					<div>
-						<Chart data={ExampleData} size={[500,500]} />
+						<Chart data={alcohol_related_crash_history} size={[500,500]} />
 					</div>
 			</div>
 		);
