1242b24daf5873e7f58a963c3e0028d06d41a134
Zhetchko Vitchev <notargetbg@gmail.com>
2018-05-15 17:16:44 +0300

    Updating of chart is possible
    - basic logic for updating chart done, including axis and scales
    - subtle styling added
    - eslinter added + simple config
    - some clearing up done

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 function getColumnNames(data) {
 	return Object.keys(data).filter(x => x !== 'year');
@@ -7,7 +7,7 @@ function getColumnNames(data) {
 
 function formatColumnNames(data) {
 	return data.map(x => {
-		return x.split("_").join(" ");
+		return x.split('_').join(' ');
 	});
 }
 
@@ -17,9 +17,18 @@ class BarChart extends React.Component {
         this.updateBarChart = this.updateBarChart.bind(this);
         this.getMax = this.getMax.bind(this);
         this.setActiveColumn = this.setActiveColumn.bind(this);
+        this.getScalesAndAxis = this.getScalesAndAxis.bind(this);
         const columns = getColumnNames(this.props.data.table[0]);
 
+        const tableData = props.data.table.map((x) => {
+            return {
+                category: x.year,
+                data: x[columns[0]]
+            };
+        });
+
         this.state = {
+            tableData,
             columns,
             activeColumn: columns[0],
             dm: {
@@ -30,107 +39,137 @@ class BarChart extends React.Component {
                 innerWidth: props.size[0] - 100,
                 innerHeight: props.size[1] - 80
             }
-        }
+        };
     }
-    componentDidMount() {        
-        this.svg = d3.select(this.svgContainerEl);       
+    componentDidMount() {
         const { dm } = this.state;
+        const { yAxis, xAxis } = this.getScalesAndAxis();
+        this.svg = d3.select(this.svgContainerEl);
 
-        this.svg.append("g")
+        this.svg.append('g')
         .attr('class', 'chart-inner')
-        .attr("width", dm.innerWidth)
-        .attr("height", dm.innerHeight)
-        .attr("transform", `translate(${dm.left},${dm.top})`);
- 
+        .attr('width', dm.innerWidth)
+        .attr('height', dm.innerHeight)
+        .attr('transform', `translate(${dm.left},${dm.top})`);
+
+         this.svg
+            .append('g')
+            .attr('class', 'axis axis--y')
+            .attr('transform', `translate(${dm.left},${dm.top})`)
+            .call(yAxis);
+
+        this.svg
+            .append('g')
+            .attr('class', 'axis axis--x')
+            .attr('transform', `translate(${dm.left},${dm.top + dm.innerHeight})`)
+            .call(xAxis);
+
+
         this.updateBarChart();
     }
     componentDidUpdate() {
         this.updateBarChart();
     }
 
-    getMax(data) {
-        return data.map((dataItem) => dataItem[this.state.activeColumn]);
-    }
+    getScalesAndAxis() {
+        const { dm, tableData } = this.state;
 
-    setActiveColumn(e) {        
-        this.setState({activeColumn: e.target.value});
-    }
-
-    updateBarChart() {
-        const { table } = this.props.data;
-        const activeColumn = this.state.activeColumn;
-        const [ sizeW, sizeH ] = this.props.size;
-        const dataMax = d3.max(this.getMax(table));
-        const { dm } = this.state;        
-        
         const yScale = d3.scaleLinear()
-            .domain([0, dataMax])
+            .domain([0, d3.max(this.getMax(tableData))])
             .range([dm.innerHeight, 0]);
 
         const xScale = d3.scaleBand()
             .range([0, dm.innerWidth]).padding(0.1)
-            .domain(table.map(item => item.year));
+            .domain(tableData.map(item => item.category));
 
         const yAxis = d3.axisLeft(yScale).ticks(20);
         const xAxis = d3.axisBottom(xScale);
 
+        return {
+            yScale,
+            xScale,
+            yAxis,
+            xAxis
+        };
+    }
+
+    getMax(data) {
+        return data.map((item) => item.data);
+    }
+
+    setActiveColumn(e) {
+        const tableData = this.props.data.table.map((x) => {
+            return {
+                category: x.year,
+                data: x[e.target.value]
+            };
+        });
+        
+        this.setState({
+            activeColumn: e.target.value,
+            tableData
+        });
+    }
+
+    updateBarChart() {
+        const { dm, tableData } = this.state;
+        const { yAxis, yScale, xAxis, xScale } = this.getScalesAndAxis();
+
         const bars = this.svg.select('.chart-inner')
             .selectAll('rect')
-            .data(table, item => item[activeColumn]);
+            .data(tableData, item => item.data);
 
         bars.enter()
             .append('rect')
+            .transition(t)
             .style('fill', '#fe9922')
-            .attr('x', item => xScale(item.year))
-            .attr('y', item => yScale(item[activeColumn]))
-            .attr('height', item => dm.innerHeight - yScale(item[activeColumn]))
-            .attr('width', xScale.bandwidth())
-            .exit()
-            .remove()
-
-        this.svg.select('.chart-inner')
-            .selectAll('text')
-            .data(table)
-            .enter()
-            .append('text')
-            .attr('text-anchor', 'middle')
-            .attr('x', item => xScale(item.year) + ( xScale.bandwidth() / 2 ))
-            .attr('y', item => yScale(item[activeColumn]) + 20)
-            .attr("font-family", "Roboto, sans-serif")
-            .attr("font-size", "15px")
-            .attr("fill", "white")
-            .text(item => item[activeColumn])
-
-        this.svg.select('.chart-inner')
-            .append('g')
-            .call(yAxis)
-
-        this.svg.select('.chart-inner')
-            .append('g')
-            .attr("class", "axis axis--x")
-            .attr('transform', `translate(0, ${dm.innerHeight})`)
-            .call(xAxis)
+            .attr('x', item => xScale(item.category))
+            .attr('y', item => yScale(item.data))
+            .attr('height', item => dm.innerHeight - yScale(item.data))
+            .attr('width', xScale.bandwidth());
 
         bars.exit()
-            .remove()
-        
+            .remove();
+
+        const barLabels = this.svg.select('.chart-inner')
+            .selectAll('text')
+            .data(tableData, item => item.data);          
+
+        barLabels
+            .enter()
+            .append('text')
+            .transition(t)
+            .attr('text-anchor', 'middle')
+            .attr('x', item => xScale(item.category) + ( xScale.bandwidth() / 2 ))
+            .attr('y', item => yScale(item.data) + 20)
+            .attr('font-family', 'Roboto, sans-serif')
+            .attr('font-size', '15px')
+            .attr('fill', 'white')
+            .text(item => item.data);
+
+        barLabels
+            .exit()
+            .remove();
+            
+        var t = d3.transition().duration(300);        
+        this.svg.select('.axis--x').transition(t).call(xAxis);
+        this.svg.select('.axis--y').transition(t).call(yAxis);
+
         }        
-    
+
     render() {
         const [ sizeW, sizeH ] = this.props.size;
         const columns = getColumnNames(this.props.data.table[0]);
 
-        console.log(this.state)
-
         return (
             <div className="chart">
                 <h3 className="chart-title">
-                    Alcohol Related Crashes
+                    {this.state.activeColumn.split('_').join(' ')}
                 </h3>
                 <div className="input-group">
 					<select onChange={this.setActiveColumn} className="form-control">
 						{columns.map((x,i) => (
-							<option key={i} value={x}>{x.split("_").join(" ")}</option>
+							<option key={i} value={x}>{x.split('_').join(' ')}</option>
 						))}
 					</select>
 				</div>
@@ -139,7 +178,7 @@ class BarChart extends React.Component {
                     width={sizeW} height={sizeH}>
                 </svg>
             </div>
-        )
+        );
     }
      
 
--- src/components/ReportingDashboard.jsx
+++ src/components/ReportingDashboard.jsx
@@ -3,28 +3,15 @@ import BarChart from './BarChart';
 
 import alcohol_related_crash_history from '../store/alcohol_related_crash_history.json';
 
-// function getColumnNames(data) {
-// 	return Object.keys(data).filter(x => x !== 'year');
-// }
-
-// function formatColumnNames(data) {
-// 	return data.map(x => {
-// 		return x.split("_").join(" ");
-// 	});
-// }
-
-// const columns = getColumnNames(alcohol_related_crash_history.table[0]);
-
 class ReportingDashboard extends React.Component {
-		
-	render() {
+	render () {
 		return (
 			<div className='container'>
 				<div className='text-center'>
 					<h2>{alcohol_related_crash_history.name}</h2>
 				</div>				
 				<div>
-					<BarChart data={alcohol_related_crash_history} size={[480,480]} />
+					<BarChart data={alcohol_related_crash_history} size={[400,400]} />
 				</div>
 			</div>
 		);
